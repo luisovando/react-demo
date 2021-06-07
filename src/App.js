9b40084ffe6aa2e9f15b747ddee5624ec6a0bade
Luis Ovando <isc.luisovando@gmail.com>
2021-06-07 02:50:56 -0500

    Update state using set state method
    Signed-off-by: Luis Ovando <isc.luisovando@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,8 +2,21 @@ import {Component} from 'react';
 import './App.css';
 
 class Counter extends Component {
-  state = {counter: 1};
-  
+  constructor () {
+    super ();
+    this.state = {counter: 1};
+    /**
+     * State facts
+     * 1. Is an inmmutable property
+     * 2. Is asynchronous
+     * 3. Just can be updated from setState method
+     */
+
+    // Update counter property every second
+    setInterval(() => {
+      this.setState({counter : this.state.counter + 1})
+    }, 1000)
+  }
   render () {
     return <span>{this.props.label} : {this.state.counter}</span>;
   }
