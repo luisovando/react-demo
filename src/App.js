8f8b178a6af72492b16b42a44d3d70eb6850657e
Luis Ovando <isc.luisovando@gmail.com>
2021-06-07 01:55:45 -0500

    Sort props alphabetically - best practice
    Signed-off-by: Luis Ovando <isc.luisovando@gmail.com>

--- src/App.js
+++ src/App.js
@@ -4,30 +4,33 @@ import { Component } from 'react';
 
 class MainTitle extends Component {
   render() {
-    return <h1>{this.props.title}</h1>
+    return <h1>{this.props.title}</h1>;
   }
+
+}
+
+class Subtitle extends Component {
+  render() {
+    return <div>
+        <p><strong>{this.props.text}</strong></p>
+        <p>Total likes: {this.props.totalLikes}</p>
+        <p>Is available?: {this.props.isAvailable}</p>
+      </div>;
+  }
+
 }
 
 function App() {
-  return (
-    <div className="App">
+  return <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <MainTitle title='Hello world from React!'/>
-        <strong>
-          Edit <code>src/App.js</code> and save to reload.
-        </strong>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <MainTitle title='Hello world from React!' />
+        <Subtitle totalLikes={2} text='Edit src/App.js and save to reload.' isAvailable={true} />
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
-    </div>
-  );
+    </div>;
 }
 
 export default App;
