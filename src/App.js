eb804dbea8e09338aed02e45a8dcdd5561d0e356
Luis Ovando <isc.luisovando@gmail.com>
2021-06-08 00:30:44 -0500

    Children props: Children layout
    Signed-off-by: Luis Ovando <isc.luisovando@gmail.com>

--- src/App.js
+++ src/App.js
@@ -3,9 +3,26 @@ import './App.css';
 
 class Box extends Component {
   render () {
-    return <div style={{border: '1px solid #090', margin: 5, padding: 5}}>
-      {this.props.children}
-    </div>;
+    return (
+      <div style={{border: '1px solid #000', margin: 5, padding: 5}}>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+class Article extends Component {
+  render () {
+    return (
+      <section>
+        <h2>{this.props.title}</h2>
+        <p><em>Author: {this.props.author}</em></p>
+        <Box>{this.props.date}</Box>
+        <article>
+          {this.props.children}
+        </article>
+      </section>
+    );
   }
 }
 
@@ -15,8 +32,14 @@ class App extends Component {
     return (
       <div className="App">
         <h4>Children props</h4>
-        <Box>Hi, I'm a children</Box>
-        <Box>Hi, I'm a other children</Box>
+        <Article
+          author="Luis"
+          date={new Date ().toLocaleDateString ()}
+          title={'The childen props'}
+        >
+          <p>The content wrapped on the Article component will send via childen props</p>
+          <strong>And will maintent it all tags and components that you added</strong>
+        </Article>
       </div>
     );
   }
