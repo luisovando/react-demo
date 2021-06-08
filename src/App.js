790d0176dba6472c60ff3eaa281a9d9904c79de4
Luis Ovando <isc.luisovando@gmail.com>
2021-06-08 00:20:22 -0500

    Children props: Basic use
    Signed-off-by: Luis Ovando <isc.luisovando@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,40 +1,22 @@
 import {Component} from 'react';
 import './App.css';
 
-class App extends Component {
-  constructor () {
-    super ();
-    this.state = {mouseX: 0, mouseY: 0};
-    // this.handleMouseMove = this.handleMouseMove.bind(this)
-  }
-
-  handleMouseMove = (e) => {
-    const {clientX, clientY} = e;
-    /**
-     * TypeError: Cannot read property 'setState' of undefined
-     * Context of `this` is wrong
-     * Fixed
-     * 1. Binding context using bind() method on constructor method: this.handleMouseMove = this.handleMouseMove.bind(this)
-     * 2. Using arrow function. This always bind the context from declaration
-     */
-    this.setState ({mouseX: clientX, mouseY: clientY});
-  }
-
-  handleClick (e) {
-    alert ('Hi, guys');
-  }
-
+class Box extends Component {
   render () {
+    return <div style={{border: '1px solid #090', margin: 5, padding: 5}}>
+      {this.props.children}
+    </div>;
+  }
+}
+
+class App extends Component {
+  render () {
+    // The children props should use to create layouts
     return (
       <div className="App">
-        <h4>Eventos</h4>
-        <button onClick={this.handleClick}>Hi, there!</button>
-        <div
-          onMouseMove={this.handleMouseMove}
-          style={{border: '1px solid #000', marginTop: 10, padding: 10}}
-        >
-          <p>{this.state.mouseX}, {this.state.mouseY}</p>
-        </div>
+        <h4>Children props</h4>
+        <Box>Hi, I'm a children</Box>
+        <Box>Hi, I'm a other children</Box>
       </div>
     );
   }
